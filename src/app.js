b1b5080055301ec566d7d7ff7d528731e442bfd2
Alexander Kochergin <monsterlessons@gmail.com>
2017-03-11 23:45:33 +0100

    Added code

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import _ from 'lodash'
+console.log('Hello webpack 2', _.isEqual(1, 2));
+
+let a = 'a';
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          }
+        }]
+      }
+    ]
+  }
+}
